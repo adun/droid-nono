78fd51d3b0fefe4bbbf09373273edcceaf27141c
adun <adun@users.noreply.github.com>
2020-10-24 15:27:07 +0200

    Display employees names in a recyclerView

---
+++ EmployeesApp/app/src/main/java/com/example/employeesapp/EmployeesAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.employeesapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.employee_item.view.*
+import org.json.JSONArray
+import org.json.JSONObject
+
+
+// Employees Adapter, used in RecyclerView in MainActivity
+class EmployeesAdapter(private val employees: JSONArray) :
+    RecyclerView.Adapter<EmployeesAdapter.ViewHolder>() {
+
+    // Create UI View Holder from XML layout
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmployeesAdapter.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.employee_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    // View Holder class to hold UI views
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val nameTextView: TextView = view.nameTextView
+    }
+
+    // Bind data to UI View Holder
+    override fun onBindViewHolder(holder: EmployeesAdapter.ViewHolder, position: Int) {
+        // employee to bind UI
+        val employee: JSONObject = employees.getJSONObject(position)
+        // employee lastname and firstname
+        holder.nameTextView.text = employee["lastName"].toString() + " " + employee["firstName"].toString()
+    }
+
+    // Return item count in employees
+    override fun getItemCount(): Int = employees.length()
+}
